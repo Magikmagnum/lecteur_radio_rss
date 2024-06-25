2effdae8b7537c3b4303a56d8ad2bea9ef7023c6
ERIC GANSA DIAMBOTE <ericgansa01@gmail.com>
2024-06-26 00:51:32 +0200

    feat: ajout de l'entity avis

--- src/Entity/Emission.php
+++ src/Entity/Emission.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\EmissionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EmissionRepository::class)]
@@ -25,6 +27,17 @@ class Emission
     #[ORM\ManyToOne(inversedBy: 'emissions')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Flux $flux = null;
+
+    /**
+     * @var Collection<int, Avis>
+     */
+    #[ORM\OneToMany(targetEntity: Avis::class, mappedBy: 'emission')]
+    private Collection $avis;
+
+    public function __construct()
+    {
+        $this->avis = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -75,6 +88,36 @@ class Emission
     public function setFlux(?Flux $flux): static
     {
         $this->flux = $flux;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Avis>
+     */
+    public function getAvis(): Collection
+    {
+        return $this->avis;
+    }
+
+    public function addAvi(Avis $avi): static
+    {
+        if (!$this->avis->contains($avi)) {
+            $this->avis->add($avi);
+            $avi->setEmission($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAvi(Avis $avi): static
+    {
+        if ($this->avis->removeElement($avi)) {
+            // set the owning side to null (unless already changed)
+            if ($avi->getEmission() === $this) {
+                $avi->setEmission(null);
+            }
+        }
 
         return $this;
     }
